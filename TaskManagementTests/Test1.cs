bcdc6519bacd839eef681e3849ab4cd9aa71263f
CristinaBoboc20 <cristinaboboc26@yahoo.com>
2025-04-27 17:53:11 +0300

    initialized test project

---
+++ TaskManagementTests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using TaskManagement.Models;
+
+namespace TaskManagementTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
